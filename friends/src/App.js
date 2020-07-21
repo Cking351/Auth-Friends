2e927348849adb9acfc68c5e2a8e2fcf1d34f1d1
Chris King <cking5836@gmail.com>
2020-07-21 17:48:19 -0400

    Created app, building components

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import Friends from './components/Friends';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Authorized Friends!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
